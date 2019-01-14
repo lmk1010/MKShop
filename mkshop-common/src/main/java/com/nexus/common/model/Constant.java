36ec63ed8676787376d8d85de86fabfce72cfb2f
lmk <lmkbnb0@gmail.com>
2019-01-14 17:19:27 +0800

    [UPDATE] 整合了Swaggar2 编写前台登陆模块

---
+++ mkshop-common/src/main/java/com/nexus/common/model/Constant.java
@@ -0,0 +1,11 @@
+package com.nexus.common.model;
+
+/**
+ * @ClassName Constant
+ * @Description TODO
+ * @Author liumingkang
+ * @Date 2019-01-14 16:37
+ * @Version 1.0
+ **/
+public class Constant {
+}
---
+++ mkshop-front-web/src/main/java/com/nexus/front/swaggar2/Swagger2Config.java
@@ -0,0 +1,51 @@
+package com.lmk.swagger;
+
+
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Auther: liumingkang
+ * @Date: 2018/9/23 15:57
+ * @Description:
+ */
+@Configuration  //让Spring来加载该类配置
+@EnableWebMvc   //非SpringBoot需启用
+@EnableSwagger2 //启用Swagger2
+public class Swagger2Config {
+    static final Logger log= LoggerFactory.getLogger(Swagger2Config.class);
+    @Bean
+    public Docket createRestApi() {
+        log.info("加载Swagger2");
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                //扫描指定包中的swagger注解
+                //.apis(RequestHandlerSelectors.basePackage("cn.exrick.controller"))
+                //扫描所有有注解的api，用这种方式更灵活
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("LMK MMall Api Documentation")
+                .description("MMALL商城API接口文档")
+                .termsOfServiceUrl("http://github.io.lmk1010")
+                .contact(new Contact("LiuMingKang", "http://github.io.lmk1010", "lmkbnb0@gmail.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
